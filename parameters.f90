ddb044bc3476fba771732401f2fb1eaba31dfe59
Valerio Briganti <brigantv@tcd.ie>
2026-07-01 12:30:52 +0200

    Corrected parameters for hartree conversion

--- parameters.f90
+++ parameters.f90
@@ -3,7 +3,7 @@ implicit none
 
  double precision, parameter     :: PI=4.D0*DATAN(1.D0)
  double precision, parameter     :: A_to_B= 1.8897259886d0
- double precision, parameter     :: Har_to_Kc=627.503d0
+ double precision, parameter     :: Har_to_Kc=627.509d0
  double precision, parameter     :: F_conv=Har_to_Kc*A_to_B
  double precision, parameter     :: boltz=3.11811E-06
  double precision, parameter    :: amu_to_emass=1822.89d0
